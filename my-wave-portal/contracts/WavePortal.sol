e96182cb83db6d77bdf6db495385fafb2627e888
yuki4san <yuki.shimura@webempath.com>
2022-02-04 00:37:26 +0900

    test

---
+++ my-wave-portal/contracts/WavePortal.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+
+    /* 乱数生成のための基盤となるシード（種）を作成 */
+    uint256 private seed;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
+    /*
+     * "address => uint mapping"は、アドレスと数値を関連付ける
+     */
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
+        console.log("We have been constructed!");
+        /*
+         * 初期シードを設定
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+        /*
+         * 現在ユーザーがwaveを送信している時刻と、前回waveを送信した時刻が15分以上離れていることを確認。
+         */
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Wait 30 seconds"
+        );
+
+        /*
+         * ユーザーの現在のタイムスタンプを更新する
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * ユーザーのために乱数を生成
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("Random # generated: %d", seed);
+
+        /*
+         * ユーザーがETHを獲得する確率を50％に設定
+         */
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            /*
+             * ユーザーにETHを送るためのコードは以前と同じ
+             */
+            uint256 prizeAmount = 0.000001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        } else {
+            console.log("%s did not win.", msg.sender);
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        return totalWaves;
+    }
+}
